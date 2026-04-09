97321f62d1a2d731cdfb7e0b368ad99603a09435
st403 <st403@WS403-12>
2026-04-09 09:30:14 +0300

    исправить тип int на string для Category

---
+++ hotel/Migrations/20260409062456_ChangeCategoryToString.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCategoryToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryNew",
+                table: "Hotel_Rooms",
+                type: "nvarchar(max)",
+                nullable: true);
+
+
+            migrationBuilder.Sql(
+                @"UPDATE Hotel_Rooms 
+          SET CategoryNew = CASE 
+              WHEN Category = 1 THEN 'Эконом'
+              WHEN Category = 2 THEN 'Стандарт'
+              WHEN Category = 3 THEN 'Люкс'
+              WHEN Category = 4 THEN 'Президентский'
+              ELSE 'Стандарт'
+          END");
+
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Hotel_Rooms");
+
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryNew",
+                table: "Hotel_Rooms",
+                newName: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomNumber",
+                table: "Hotel_Room",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Hotel_Room",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Floor",
+                table: "Hotel_Room",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Hotel_Room",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
